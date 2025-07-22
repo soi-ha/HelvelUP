9bf828ea8f4ac1a0fde7d640ff73de307dda437b
Soyeon Choe <soy2302ten@gmail.com>
2025-07-22 12:21:50 +0900

    misc: eslint 및 prettier 환경 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: 'HevelUP',
-	description: 'Health Level UP! 나만의 운동 RPG',
+  title: "HevelUP",
+  description: "Health Level UP! 나만의 운동 RPG",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ko">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="ko">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-	return <div>helvelUP</div>;
+  return <div>helvelUP</div>;
 }
